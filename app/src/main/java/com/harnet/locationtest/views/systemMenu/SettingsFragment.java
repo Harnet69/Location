5bdb463615552b62c9f78454bc79b926f75406fe
Harnet69 <harnet69@gmail.com>
2020-08-31 09:28:36 +0200

    Implemented progress bar

--- app/src/main/java/com/harnet/locationtest/views/systemMenu/SettingsFragment.java
+++ app/src/main/java/com/harnet/locationtest/views/systemMenu/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.harnet.locationtest.views.systemMenu;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,16 +14,19 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.harnet.locationtest.R;
 import com.harnet.locationtest.services.PlacesService;
 
 public class SettingsFragment extends Fragment {
+    private Context context;
+
     private SwitchCompat darkModeSwitch;
     private SwitchCompat muteSoundsSwitch;
     private SwitchCompat sqliteSwitch;
-    private Context context;
+    private ProgressBar progressBar;
 
     public SettingsFragment() {
     }
@@ -40,6 +44,7 @@ public class SettingsFragment extends Fragment {
         darkModeSwitch = (SwitchCompat) view.findViewById(R.id.darkMode_switch);
         muteSoundsSwitch = (SwitchCompat) view.findViewById(R.id.muteSounds_switch);
         sqliteSwitch = (SwitchCompat) view.findViewById(R.id.SQLite_switch);
+        progressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
 
         SQLiteSwitch(sqliteSwitch);
 
@@ -50,31 +55,40 @@ public class SettingsFragment extends Fragment {
     //set listener to SQLite switch
     public void SQLiteSwitch(SwitchCompat SQLiteSwitcher) {
         SQLiteSwitcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("StaticFieldLeak")
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 PlacesService.getInstance(context).switchSQLite();
                 boolean isSQLite = PlacesService.getInstance(context).isSQLite();
 
+                progressBar.setVisibility(View.VISIBLE);
                 //TODO make a migration from/to SQLite
                 new AsyncTask<Void, Void, Void>() {
                     @Override
                     protected void onPostExecute(Void aVoid) {
                         super.onPostExecute(aVoid);
-                        //TODO make a spinner to show migration is work
-                    }
-
-                    @Override
-                    protected Void doInBackground(Void... voids) {
                         if(!isSQLite) {
                             PlacesService.getInstance(context).migrateToSharedPreferences();
                         }else{
                             PlacesService.getInstance(context).migrateToSQLite();
                         }
+                        Toast.makeText(context, "SQLite " + isSQLite, Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        //delaying for spinner
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         return null;
                     }
                 }.execute();
-
-                Toast.makeText(context, "SQLite " + isSQLite, Toast.LENGTH_SHORT).show();
+//
+//                Toast.makeText(context, "SQLite " + isSQLite, Toast.LENGTH_SHORT).show();
             }
         });
     }
